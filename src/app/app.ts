a825a0224dc2108ec11eebd00ac342bddcafbc05
Patrick McG <patrickdmcgurnaghan@gmail.com>
2025-11-11 14:25:04 +0000

    busineses

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Businesses} from './componants/businesses/businesses';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Businesses],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
