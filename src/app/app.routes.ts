162a5ef347e7fcd791029b75bbaebe0efe294105
Patrick McG <patrickdmcgurnaghan@gmail.com>
2025-11-11 16:18:52 +0000

    adds basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './componants/home/home'
+import { Businesses} from './componants/businesses/businesses';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },{
+  path: 'businesses',
+    component: Businesses,
+  }
+];
